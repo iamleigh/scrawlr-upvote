02a4b29dfd66bd196402017e83d7f17eac624799
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2025-06-20 03:34:39 -0500

    chore: Add path aliases for components
    - Configure aliases in vite.config.ts using path.resolve
    - Add @types/node for proper Node.js module typing
    - Enable cleaner and more maintainable imports across the project

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './'),
+      '@atom': path.resolve(__dirname, './src/components/atoms'),
+      '@molecule': path.resolve(__dirname, './src/components/molecules'),
+      '@organism': path.resolve(__dirname, './src/components/organisms'),
+    }
+  },
   plugins: [react()],
 })
