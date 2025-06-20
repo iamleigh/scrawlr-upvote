a2eddec02923f624dda9b4c7efa6a1f039881fc5
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2025-06-20 04:13:25 -0500

    feat(icon): Create atom component
    - Create `Icon.tsx` for rendering “FontAwesome” icons
    - Define reusable `IconName` type and `iconMap`
    - Use `type-only` imports to comply with `verbatimModuleSyntax`

---
+++ src/components/atoms/Icon/Icon.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { iconMap } from "../../../types/icon"
+import type { IconName } from "../../../types/icon"
+
+interface IconProps {
+    name: IconName
+}
+
+const Icon: React.FC<IconProps> = ({ name }) => {
+    return <FontAwesomeIcon icon={iconMap[name.toLowerCase() as keyof typeof iconMap]} />
+}
+
+export default Icon
---
+++ src/types/icon.ts
@@ -0,0 +1,13 @@
+import { faArrowUp, faAdd } from "@fortawesome/free-solid-svg-icons"
+
+/// -----------------------------------------------------------------------------
+/// 🟢 IconName
+///
+/// Represents the names of icons available in the icon map.
+/// -----------------------------------------------------------------------------
+export const iconMap = {
+    up: faArrowUp,
+    add: faAdd,
+}
+
+export type IconName = keyof typeof iconMap
