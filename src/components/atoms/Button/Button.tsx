16d661fb4a52a221c7ebd98695076191e3084d95
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2025-06-20 10:45:55 -0500

    feat(button): Create atom component
    - Create `Button` component with support for label, icon, and toggle behavior
    - Style button with SCSS Modules, including hover, focus-visible, and selected states
    - Refactor index styles for consistency
    - Install required dependencies

---
+++ src/components/atoms/Button/Button.tsx
@@ -0,0 +1,40 @@
+import React from "react"
+import Icon from "../Icon/Icon"
+import type { IconName } from "../../../types/icon"
+import styles from "./button.module.scss"
+import clsx from "clsx"
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+    label: string
+    icon?: IconName
+    hideLabel?: boolean
+    selected?: boolean
+    disabled?: boolean
+    onClick?: React.MouseEventHandler<HTMLButtonElement>
+}
+
+const Button: React.FC<ButtonProps> = ({
+    label,
+    icon,
+    hideLabel,
+    selected,
+    disabled,
+    onClick
+}) => {
+    return (
+        <button
+            className={clsx({
+                [styles['suc-button']]: true,
+                [styles['suc-button--selected']]: selected,
+            })}
+            {...(hideLabel && { "aria-label": label })}
+            {...(typeof selected === "boolean" && { "aria-pressed": selected })}
+            {...(disabled && { "disabled": true })}
+            onClick={onClick}>
+            {icon && <Icon name={icon} />}
+            {!hideLabel && label}
+        </button>
+    )
+}
+
+export default Button
