d4466fcf9763d1fd111cb3826a7ab9843ff2d4b2
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2025-06-22 10:25:08 -0500

    refactor: Split `UpvoteList` logic into standalone `UpvotePanel` container
    - Created new `UpvotePanel` organism to manage upvote state and button logic
    - Moved dynamic count and selection state from `UpvoteList` into `UpvotePanel`
    - Delegated rendering responsibility to `UpvoteList` as a presentational component
    - Included "Add Upvote" button inside `UpvotePanel` to keep behavior centralized
    - Improved code structure and separation of concerns following atomic design

---
+++ src/components/organisms/UpvotePanel/UpvotePanel.tsx
@@ -0,0 +1,27 @@
+import React from "react"
+import Button from "../../atoms/Button/Button"
+import UpvoteList from "../../molecules/UpvoteList/UpvoteList"
+import styles from "./upvotePanel.module.scss"
+import clsx from "clsx"
+
+const UpvotePanel: React.FC = () => {
+    const [upvotes, setUpvotes] = React.useState<number>(1)
+    const [selected, setSelected] = React.useState<boolean>(false)
+
+    return (
+        <div className={clsx(styles['suc-upvote-panel'])}>
+            <UpvoteList
+                upvotes={upvotes}
+                selected={selected}
+                onToggle={() => setSelected(!selected)} />
+
+            <Button
+                label="Add Upvote"
+                icon="add"
+                hideLabel={true}
+                onClick={() => setUpvotes(upvotes+1)} />
+        </div>
+    )
+}
+
+export default UpvotePanel
