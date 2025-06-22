3db9e51f5cfd007e09c9638cb3f87cf9715ea8de
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2025-06-22 12:50:18 -0500

    chore: Config and sync path aliases for Vite and TS
    - Added module path aliases to `tsconfig.app.json` for TypeScript and IDE support
    - Ensured consistency with alias paths already defined in `vite.config.ts`
    - Fixed “IntelliSense” and type resolution issues in VSCode by aligning `paths` with the active `tsconfig`
    - Confirmed aliases work both at runtime and during development with `vite-tsconfig-paths`

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import path from 'path'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vite.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './'),
-      '@atom': path.resolve(__dirname, './src/components/atoms'),
-      '@molecule': path.resolve(__dirname, './src/components/molecules'),
-      '@organism': path.resolve(__dirname, './src/components/organisms'),
-    }
-  },
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
 })
