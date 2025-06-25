f857b9971115148abaa0b1ff0fe1db881c6747f0
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2025-06-25 04:52:29 -0500

    feat(header): Improve the component styles

--- src/components/molecules/Header/Header.tsx
+++ src/components/molecules/Header/Header.tsx
@@ -1,4 +1,6 @@
 import type React from 'react'
+import styles from './Header.module.scss'
+import clsx from 'clsx'
 
 interface HeaderProps {
     title?: string
@@ -7,9 +9,9 @@ interface HeaderProps {
 
 const Header: React.FC<HeaderProps> = ({ title, children }) => {
     return (
-        <header>
-            {title && <h1>{title}</h1>}
-            {children && <div>{children}</div>}
+        <header className={clsx({ [styles['suc-header']]: true })}>
+            {title && <h1 className={clsx({ [styles['suc-header__title']]: true })}>{title}</h1>}
+            {children && <div className={clsx({ [styles['suc-header__actions']]: true })}>{children}</div>}
         </header>
     )
 }
