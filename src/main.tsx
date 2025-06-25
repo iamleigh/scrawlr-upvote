60ac101987a617d11558a2597ebf58780ba3c56b
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2025-06-25 04:42:02 -0500

    refactor: Create `@styles` alias

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import './assets/scss/main.scss'
+import '@styles/main.scss'
 
 createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
