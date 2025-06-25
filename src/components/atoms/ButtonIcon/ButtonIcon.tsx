a97cf13512158991ef3e06056a49b71ce3cd7a48
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2025-06-25 06:25:26 -0500

    feat(button-icon): Create atom component

---
+++ src/components/atoms/ButtonIcon/ButtonIcon.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import Icon from '@atom/Icon/Icon'
+import type { IconName } from '@atom/Icon/icon.types'
+import styles from './ButtonIcon.module.scss'
+import clsx from 'clsx'
+
+import type { ButtonSize } from '../Button/button.types'
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+    label: string
+    icon: IconName
+    size?: ButtonSize
+    selected?: boolean
+    disabled?: boolean
+    onClick?: React.MouseEventHandler<HTMLButtonElement>
+}
+
+const ButtonIcon: React.FC<ButtonProps> = ({
+    label,
+    icon,
+    size = "md",
+    selected,
+    disabled,
+    onClick,
+}) => {
+    return (
+        <button
+            className={clsx({
+                [styles['suc-button-icon']]: true,
+                [styles['suc-button-icon--sm']]: size === 'sm',
+                [styles['suc-button-icon--lg']]: size === 'lg',
+                [styles['suc-button-icon--selected']]: selected,
+            })}
+            aria-label={label}
+            {...(typeof selected === 'boolean' && { 'aria-pressed': selected })}
+            {...(disabled && { disabled: true })}
+            onClick={onClick}
+        >
+            <Icon name={icon} size={size} />
+        </button>
+    )
+}
+
+export default ButtonIcon
